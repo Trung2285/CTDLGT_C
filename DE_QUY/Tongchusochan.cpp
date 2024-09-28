d0651da9fb8973f5290497840399b57eec1430fc
Le Van Trung <levantrung220805@gmail.com>
2024-09-28 11:47:26 +0700

    DeQuy

---
+++ DE_QUY/Tongchusochan.cpp
@@ -0,0 +1,18 @@
+//Đệ quy tính tổng chữ số chẵn
+#include<stdio.h>
+int tongChan(int n)
+{
+	if(n%2==0)
+//		if(n<10)
+//			return n;
+//		else
+			return n%10 + tongChan(n/10);
+	else
+		return tongChan(n/10);
+}
+main()
+{
+	int n;
+	scanf("%d",&n);
+	printf("Tong chu so chan la : %d",tongChan(n));
+}
